ea376c76df8ad555fdf8f74e6d31b5c3f192c66d
Keanu Barnard <keanu.barnardd@gmail.com>
2022-12-21 21:58:46 +1100

    Project restart

---
+++ IMS/IMS.UserCases/Inventories/ViewInventoriesUseCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UserCases.Inventories
+{
+  public class ViewInventoriesByNameUseCase
+  {
+    public IEnumerable<Inventory> ExecuteAsync(string name = "")
+    {
+
+    }
+  }
+}
